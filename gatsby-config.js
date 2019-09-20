800c6938dcceb5b03b994c266ac01ce28b17dd2c
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-09-20 15:48:50 -0400

    Added favicon, and other assets to be used. Added styling to 404 page. Began media queries for Workshops page.

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,22 +67,26 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Virtual Grounds Workshop Series`,
+        short_name: `VirtualGrounds`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#307574`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/femmeTechIcon.png`,
       },
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+
+
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+
+
   ],
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,29 @@ import React from "react"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
+import FemmeInLargeCoat from "../components/images/femmeInLargeCoat"
+
+import componentStyles from "./404.module.scss"
 
 const NotFoundPage = () => (
-  <Layout bodyClass="greenBody">
+  <Layout bodyClass="orangeBody">
     <SEO title="404: Not found"/>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    
+    <section className={componentStyles.block}>
+      
+      <div>
+        <h1>404</h1>
+        <h2>not found</h2>
+      </div>
+      
+      <div>
+        <FemmeInLargeCoat />
+      </div>
+      
+
+    </section>
+    
+
   </Layout>
 )
 
